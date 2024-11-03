213afb4e8d3215974521ccd65c318d4d0b8e73a3
praveendran-tech <praveend@umd.edu>
2024-11-02 20:00:28 -0400

    Initial commit

---
+++ src/test/java/com/github/ilim/backend/ilimEmailDeliveryServ/IlimEmailDeliveryServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ilim.backend.ilimEmailDeliveryServ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlimEmailDeliveryServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
